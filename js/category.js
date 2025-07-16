275301af8cf200b7d55f785d1080bab0eb1253b7
aadeyetwhya <adityakafle@gmail.com>
2025-07-16 12:09:54 +0545

    final commit

--- js/category.js
+++ js/category.js
@@ -33,17 +33,17 @@ const categoryCard = (eachBook, selectedElement) => {
                     <div class="bookInfo">
                         
                         <h2 class="title">
-                            ${eachBook.volumeInfo.title}
+                            ${eachBook?.volumeInfo?.title || "No title available"}
                         </h2>
 
                        <i> <p class="author">
-                            ${eachBook.volumeInfo.authors.map(eachAuthor=> eachAuthor).join(" , ") }
+                            ${eachBook?.volumeInfo?.authors?.map(eachAuthor=> eachAuthor).join(" , ") || "NO author mentioned" }
                         </p></i>
                         <p class="year">
                             ${eachBook.volumeInfo.publishedDate}
                         </p>
                         <p class="description">
-                            ${eachBook.volumeInfo.description?.slice(0,180)+"..."}
+                            ${eachBook?.volumeInfo?.description?.slice(0,180)||"NO description "+"..."}
                         </p>
                 </div>`
     
